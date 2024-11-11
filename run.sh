8f9c460e7ee2b5fffe7a6182f688cf783c736d59
Katie Keith <kakeith406@gmail.com>
2024-11-11 15:48:48 -0500

    update

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:1        # Request one GPUs
 #SBATCH -t 0-02:00          # Runtime in D-HH:MM, minimum of 10 mins (this requests 2 hours)
 #SBATCH -p dl               # Partition to submit to (should always be dl, for now)
-#SBATCH --mem=2G           # Request 10G of memory
+#SBATCH --mem=2G            # Request 2G of memory
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written (%j inserts jobid)
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written (%j inserts jobid)
 

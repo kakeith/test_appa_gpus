74cb5ae49a7b245975929c1c48662e8192490911
Katherine A. Keith <kakeith@users.noreply.github.com>
2024-11-18 10:34:28 -0500

    Update run_on_gpmoo.sh

--- run_on_gpmoo.sh
+++ run_on_gpmoo.sh
@@ -2,7 +2,7 @@
 #SBATCH -c 1                # Request 1 CPU core
 #SBATCH --gres=gpu:1        # Request one GPUs
 #SBATCH -t 0-02:00          # Runtime in D-HH:MM, minimum of 10 mins (this requests 2 hours)
-#SBATCH -p gpmoo-b               # Partition to submit to (should always be dl, for now)
+#SBATCH -p gpmoo-b          # Partition to submit to
 #SBATCH --mem=2G            # Request 2G of memory
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written (%j inserts jobid)
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written (%j inserts jobid)

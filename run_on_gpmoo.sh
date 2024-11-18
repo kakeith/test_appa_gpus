f5ded082bcefd43bba44afccee8edbd0186b04af
Katherine A. Keith <kakeith@users.noreply.github.com>
2024-11-18 10:34:16 -0500

    Update run_on_gpmoo.sh

--- run_on_gpmoo.sh
+++ run_on_gpmoo.sh
@@ -2,7 +2,7 @@
 #SBATCH -c 1                # Request 1 CPU core
 #SBATCH --gres=gpu:1        # Request one GPUs
 #SBATCH -t 0-02:00          # Runtime in D-HH:MM, minimum of 10 mins (this requests 2 hours)
-#SBATCH -p gpmoo-a               # Partition to submit to (should always be dl, for now)
+#SBATCH -p gpmoo-b               # Partition to submit to (should always be dl, for now)
 #SBATCH --mem=2G            # Request 2G of memory
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written (%j inserts jobid)
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written (%j inserts jobid)
@@ -10,5 +10,4 @@
 # Command you want to run on the cluster
 # Notice, you must set-up testEval correctly as a conda virtual environment
 # Calling this full path makes sure you are running the correct package versions
-#~/anaconda3/envs/testEnv/bin/python3.9 train_net.py
 ~/.conda/envs/testEnv/bin/python3.9 train_net_w_gpus.py
